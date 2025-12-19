afe8dc4f5fc3f25ed8ffbdac38ac4a455062e880
paulgavriloaea <168186652+paulgavriloaea@users.noreply.github.com>
2025-12-19 21:20:11 +0200

    Insert and format sales data into crm_sales_details
    This script truncates the crm_sales_details table and inserts new sales data with proper date formatting and sales calculations.

--- scripts/silver/insert_data.sql
+++ scripts/silver/insert_data.sql
@@ -58,3 +58,33 @@ SELECT
 ) AS prd_end_dt
 FROM crm_prd_info;
 
+
+TRUNCATE TABLE DataWarehouse_silver.crm_sales_details;
+
+
+INSERT INTO DataWarehouse_silver.crm_sales_details(sls_ord_num, sls_prd_key, sls_cust_id, sls_order_dt,
+sls_ship_dt, sls_due_dt, sls_sales, sls_quantity, sls_price)
+
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt) != 8 THEN NULL
+		ELSE STR_TO_DATE(sls_order_dt, '%Y%m%d')
+	END AS sls_order_dt,
+	CASE WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt) != 8 THEN NULL
+		ELSE STR_TO_DATE(sls_ship_dt, '%Y%m%d')
+	END AS sls_ship_dt,
+	CASE WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt) != 8 THEN NULL
+		ELSE STR_TO_DATE(sls_due_dt, '%Y%m%d')
+	END AS sls_due_dt,
+	CASE WHEN sls_sales IS NULL OR sls_sales<=0 OR sls_sales != sls_quantity*ABS(sls_price)
+		THEN sls_quantity*ABS(sls_price) 
+        ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE WHEN sls_price IS NULL or sls_price<=0 THEN sls_sales/NULLIF(sls_quantity,0)
+		ELSE sls_price
+	END AS sls_price
+FROM crm_sales_details;
+
