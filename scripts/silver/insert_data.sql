5f49cf2040d4152c01e549f0bf742bd2ea20b4fa
paulgavriloaea <168186652+paulgavriloaea@users.noreply.github.com>
2025-12-19 20:40:25 +0200

    Insert data into crm_prd_info table
    Added data insertion logic for crm_prd_info table.

--- scripts/silver/insert_data.sql
+++ scripts/silver/insert_data.sql
@@ -31,3 +31,30 @@ SELECT
 ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
 FROM crm_cust_info)t
 WHERE flag_last=1;
+
+TRUNCATE TABLE DataWarehouse_silver.crm_prd_info;
+
+INSERT INTO DataWarehouse_silver.crm_prd_info(prd_id,cat_id,prd_key,prd_nm,
+prd_cost,prd_line,prd_start_dt,prd_end_dt)
+
+SELECT
+	prd_id,
+    REPLACE(SUBSTRING(prd_key, 1, 5), '-','_') AS cat_id,
+    SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key, 
+    prd_nm,
+    IFNULL(prd_cost,0) AS prd_cost,
+    CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+         WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+         WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'other Sales'
+         WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+         ELSE 'n/a'
+	END AS prd_line,
+    CAST(prd_start_dt AS DATE) AS prd_start_dt,
+    CAST(
+    DATE_SUB(
+        LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt),
+        INTERVAL 1 DAY
+    ) AS DATE
+) AS prd_end_dt
+FROM crm_prd_info;
+
