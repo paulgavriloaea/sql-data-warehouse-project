077c303069cf0c5c391f67150f42e1da5ffb8840
paulgavriloaea <168186652+paulgavriloaea@users.noreply.github.com>
2026-01-01 17:45:27 +0200

    Update insert_data.sql

--- scripts/silver/insert_data.sql
+++ scripts/silver/insert_data.sql
@@ -96,9 +96,10 @@ END AS cid,
 CASE WHEN bdate > NOW() THEN NULL
 	ELSE bdate
 END AS bdate,
-CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-	 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-     ELSE 'n/a'
+CASE
+  WHEN UPPER(TRIM(gen)) LIKE 'F%' THEN 'Female'
+  WHEN UPPER(TRIM(gen)) LIKE 'M%' THEN 'Male' 
+  ELSE 'n/a'
 END AS gen
 FROM DataWarehouse_bronze.erp_cust_az12;
 
