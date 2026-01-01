b0eb995dad544eee40c09a2b1103cb658f809d75
paulgavriloaea <168186652+paulgavriloaea@users.noreply.github.com>
2026-01-01 16:07:51 +0200

    Insert data into erp_loc_a101 with country mapping

--- scripts/silver/insert_data.sql
+++ scripts/silver/insert_data.sql
@@ -105,3 +105,16 @@ CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
      ELSE 'n/a'
 END AS gen
 FROM DataWarehouse_bronze.erp_cust_az12;
+
+TRUNCATE DataWarehouse_silver.erp_loc_a101;
+
+INSERT INTO DataWarehouse_silver.erp_loc_a101 (cid,cntry)
+
+SELECT
+REPLACE(cid,'-','') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+     WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+     ELSE TRIM(cntry)
+END AS cntry
+FROM DataWarehouse_bronze.erp_loc_a101;
