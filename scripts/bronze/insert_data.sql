5080a3668495ae6c6b216e6b9dc2e0690396e8f1
paulgavriloaea <168186652+paulgavriloaea@users.noreply.github.com>
2026-01-08 20:54:38 +0200

    Update file paths in insert_data.sql

--- scripts/bronze/insert_data.sql
+++ scripts/bronze/insert_data.sql
@@ -21,7 +21,7 @@ TRUNCATE TABLE erp_cust_az12;
 TRUNCATE TABLE erp_loc_a101;
 TRUNCATE TABLE erp_px_cat_g1v2;
   
-LOAD DATA LOCAL INFILE '../../source_crm/datasets/sales_details.csv'
+LOAD DATA LOCAL INFILE '../../datasets/source_crm/sales_details.csv'
 INTO TABLE crm_sales_details
 FIELDS TERMINATED BY ',' -- column separator
 LINES TERMINATED BY '\n'
@@ -30,7 +30,7 @@ IGNORE 1 LINES
 sls_due_dt, sls_sales, sls_quantity, sls_price);
 
 
-LOAD DATA LOCAL INFILE '../../source_crm/datasets/cust_info.csv'
+LOAD DATA LOCAL INFILE '../../datasets/source_crm/cust_info.csv'
 INTO TABLE crm_cust_info
 FIELDS TERMINATED BY ',' -- column separator
 LINES TERMINATED BY '\n'
@@ -39,7 +39,7 @@ IGNORE 1 LINES
 cst_marital_status,cst_gndr,cst_create_date);
 
 
-LOAD DATA LOCAL INFILE '../../source_crm/datasets/prd_info.csv'
+LOAD DATA LOCAL INFILE '../../datasets/source_crm/prd_info.csv'
 INTO TABLE crm_prd_info
 FIELDS TERMINATED BY ',' -- column separator
 LINES TERMINATED BY '\n'
@@ -47,21 +47,21 @@ IGNORE 1 LINES
 (prd_id,prd_key,prd_nm,prd_cost,prd_line,prd_start_dt,prd_end_dt);
 
 
-LOAD DATA LOCAL INFILE '../../source_crm/datasets/source_erp/CUST_AZ12.csv'
+LOAD DATA LOCAL INFILE '../../datasets/source_erp/CUST_AZ12.csv'
 INTO TABLE erp_cust_az12
 FIELDS TERMINATED BY ',' -- column separator
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (cid,bdate,gen);
 
-LOAD DATA LOCAL INFILE '../../source_crm/datasets/source_erp/LOC_A101.csv'
+LOAD DATA LOCAL INFILE '../../datasets/source_erp/LOC_A101.csv'
 INTO TABLE erp_loc_a101
 FIELDS TERMINATED BY ',' -- column separator
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (cid,cntry);
 
-LOAD DATA LOCAL INFILE '../../source_crm/datasets/source_erp/PX_CAT_G1V2.csv'
+LOAD DATA LOCAL INFILE '../../datasets/source_erp/PX_CAT_G1V2.csv'
 INTO TABLE erp_px_cat_g1v2
 FIELDS TERMINATED BY ',' -- column separator
 LINES TERMINATED BY '\n'
